b5a275495aec96bdd61f17b6a9950d49fc2a54b5
.email.com <gangisettymani98@gmail.com>
2026-08-17 16:00:03 +0530

    if i create terraform roboshop-dev- info this time so i have create by terraform

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source      = "git ::https://github.com/gangisettymanikumar79-oss/vpc-test.git"? ref=main # <-- correct path
+  source      = "git ::https://github.com/gangisettymanikumar79-oss/vpc-test.git? ref=main" # <-- correct path
   project     = "roboshop"
   environment = "dev"
   is_peering_required = true
